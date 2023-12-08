602d29ff7c17306892e48b9917fea07aab6af681
apatwal <107596781+apatwal@users.noreply.github.com>
2023-12-07 21:25:10 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('message', (data) => {
+    io.emit('newMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
